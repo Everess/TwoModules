b1be95014aa70eb31fe5f8e2292e8b0f506e1de2
Everess <everess1@mail.ru>
2019-06-14 19:59:40 +0400

    Test delete

--- SecondModule/build/resources/main/db/V1__init.sql
+++
@@ -1,9 +0,0 @@
-create table usr (
-    id int8 not null,
-    activation_code varchar(255),
-    active boolean not null,
-    email varchar(255),
-    password varchar(255) not null,
-    username varchar(255) not null,
-    primary key (id)
-);
